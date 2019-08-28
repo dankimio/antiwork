0ce3613ec8c0c576323fc84f5d2f5805b802d1d2
Dan Kim <itsdanya@gmail.com>
2019-08-28 19:18:08 +0300

    Create JobsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   mount Lockup::Engine, at: '/lockup'
-
-  passwordless_for :users
+  passwordless_for :companies
 
   root 'pages#index'
+
+  resources :jobs, only: %i[new]
 
   get 'new-advert', to: 'pages#new'
   get 'job-description', to: 'pages#job_description'
   get 'company-info', to: 'pages#company_info'
   get 'payment', to: 'pages#payment'
   get 'show', to: 'pages#show'
-  get 'sign-in', to: 'pages#sign_in'
+  # get 'sign-in', to: 'pages#sign_in'
 end
