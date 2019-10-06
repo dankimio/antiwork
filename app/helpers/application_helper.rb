99a0e57c1d3ba703e1111535b86fe08ad058167d
Dan Kim <itsdanya@gmail.com>
2019-10-06 22:23:35 +0300

    Add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def title(custom_title = nil)
+    page_title = custom_title || t("#{controller_name}.#{action_name}.title", default: '')
+    base_title = "#{ENV['HOSTNAME'] || 'anti.work'}"
+
+    if page_title.present?
+      page_title + ' — ' + base_title
+    else
+      base_title
+    end
+  end
 end
