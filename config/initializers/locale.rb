57a4f50689e63258c6e1753487039c20a678d2f1
Dan Kim <itsdanya@gmail.com>
2019-10-16 21:07:05 +0300

    Add locale initializer with available locales and default locale

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,1 +1,5 @@
-I18n.available_locales = [:en, :ru]
+# Permitted locales available for the application
+I18n.available_locales = %i[en ru]
+
+# Set default locale to something other than :en
+I18n.default_locale = :ru
