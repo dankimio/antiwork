38b4459de09b5278e83a54bacbabf9e036f64ec3
Dan Kim <itsdanya@gmail.com>
2019-12-30 00:26:34 +0300

    Fix jobs#index test

--- test/controllers/jobs_controller_test.rb
+++ test/controllers/jobs_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class JobsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get index' do
+    get jobs_url
+    assert_response :success
+  end
+
+  test 'should get new' do
     get new_job_url
     assert_response :success
   end
